518c7e37aa9a40ab42caafb41bc8ff4a35c3ba98
Aman Kumar Singh <amankumarsingh.professional@gmail.com>
2022-01-04 12:38:16 +0530

    Add challenge files

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center"><img src="https://user-images.githubusercontent.com/73401649/148022730-5e42b154-d30f-40d9-ac45-a7320f5b6313.png" alt="JARVIS" width="100%"/></a></p>
+
+## 📌 Introduction
+This is a python telegram bot challenge.
+
+## 🏃‍ Getting Started
+- Fork this repository and then clone it.
+- Create a python virtual environment in the directory, and activate it:
+  ```sh
+  python3 -m venv .
+  source bin/activate
+  ```
+- Install required packages:
+  ```sh
+  pip3 install -r requirements.txt
+  ```
+- To run the bot (for testing):
+  ```sh
+  python3 bot.py
+  ```
+
+## 🧱 Stages
+   - **Stage 1:** Fetching Nutrition Data from the Nutritionix API and displaying it to telegram chatbox.
+   
+      - 3 TODOs from 1.1 to 1.3. 
+  
+  - **Stage 2:** Fetching Exercise Data from the Nutritionix API and displaying it to telegram chatbox.
+
+      - 4 TODOs from 2.1 to 2.4.
+  
+  - **Stage 3:** Dumping the Nutrition and Exercises Data to the CSV File.
+
+      - 4 TODOs from 3.1 to 3.4.
+
+  - **Stage 4:** **OPTIONAL** **(Get extra karma points)** Deploy the bot to Heroku.
+
+## 🎓 Resources
+- [Nutritionix API](https://www.nutritionix.com/)
+- [pyTelegramBotAPI](https://pypi.org/project/pyTelegramBotAPI/0.3.0/)
+- [BotFather](https://medium.com/shibinco/create-a-telegram-bot-using-botfather-and-get-the-api-token-900ba00e0f39)
+
+## 📝 Submission Guidelines
+- Checkout to a branch with your name.
+- Make a PR to the main branch.
+
+## 👀 Live Bot Preview
+- [TeleFit Bot Link](https://t.me/Tele_Fit_Bot)
---
+++ bot.py
@@ -0,0 +1,79 @@
+import os
+from os import environ
+import telebot
+import requests
+import json
+import csv
+
+# TODO: 1.1 Add Request HTTP URL of the API
+NUTRITIONIX_API_KEY = environ['NUTRITIONIX_API_KEY']
+NUTRITIONIX_APP_ID = environ['NUTRITIONIX_APP_ID']
+HTTP_API = environ['http_api']
+
+headers = {'Content-Type': 'application/json',
+           'x-app-id': NUTRITIONIX_APP_ID, 'x-app-key': NUTRITIONIX_API_KEY}
+user = {'name': None, 'gender': None,
+        'weight': None, 'height': None, 'age': None}
+bot = telebot.TeleBot(HTTP_API)
+
+
+@bot.message_handler(commands=['start', 'hello'])
+def greet(message):
+    global botRunning
+    botRunning = True
+    # TODO: 3.1 Add CSV file creation
+    bot.reply_to(
+        message, 'Hello! I am TeleFit. Use me to monitor your health'+'\N{grinning face with smiling eyes}'+'\nYou can use the command \"/help\" to know more about me.')
+
+
+@bot.message_handler(commands=['stop', 'bye'])
+def goodbye(message):
+    global botRunning
+    botRunning = False
+    bot.reply_to(message, 'Bye!\nStay Healthy'+'\N{flexed biceps}')
+
+
+@bot.message_handler(func=lambda message: botRunning, commands=['help'])
+def helpProvider(message):
+    bot.reply_to(message, '1.0 You can use \"/nutrition Units Quantity-Type Food-Name\" command to get the nutrients of a particular food. For eg: \"/nutrition 1 piece chapati\"\n\n2.1 For using the bot to get details about an exercise you need to first set the user data using the command \"/user Name, Gender, Weight(in Kg), Height (in cm), Age\". For eg: \"/user Akshat, Male, 70, 6, 19\n\n2.2 Then you can use the command \"/execise Duration-amount Duration-unit Exercise-name\" to get data about an exercise. For eg: \"/exercise 40 minutes push-ups\"\n\n3.0. You can use the command \"/reports Report-name\" to get the reports in CSV Format. For eg: \"/reports nutrition\" to get nutrition report and \"/reports exercise\" to get exercise reports or use the command \"/reports nutrition, exercise\" to get both nutrition and exercise reports\n\n4.0. You can use the command \"/stop\" or the command \"/bye\" to stop the bot.')
+
+
+@bot.message_handler(func=lambda message: botRunning, commands=['user'])
+def setUser(message):
+    global user
+    usr_input = message.text[6:]
+    # TODO: 2.1 Set user data
+    bot.reply_to(message, 'User set!')
+    reply = ''
+    # TODO: 2.2 Display user details in the telegram chat
+    bot.send_message(message.chat.id, reply)
+
+
+@bot.message_handler(func=lambda message: botRunning, commands=['nutrition'])
+def getNutrition(message):
+    bot.reply_to(message, 'Getting nutrition info...')
+    # TODO: 1.2 Get nutrition information from the API
+    # TODO: 1.3 Display nutrition data in the telegram chat
+    # TODO: 3.2 Dump data in a CSV file
+
+
+@bot.message_handler(func=lambda message: botRunning, commands=['exercise'])
+def getCaloriesBurn(message):
+    bot.reply_to(message, 'Estimating calories burned...')
+    # TODO: 2.3 Get exercise data from the API
+    # TODO: 2.4 Display exercise data in the telegram chat
+    # TODO: 3.3 Dump data in a CSV file
+
+
+@bot.message_handler(func=lambda message: botRunning, commands=['reports'])
+def getCaloriesBurn(message):
+    bot.reply_to(message, 'Generating report...')
+    # TODO: 3.4 Send downlodable CSV file to telegram chat
+
+
+@bot.message_handler(func=lambda message: botRunning)
+def default(message):
+    bot.reply_to(message, 'I did not understand '+'\N{confused face}')
+
+
+bot.infinity_polling()
