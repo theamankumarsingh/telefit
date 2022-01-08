62a948bf5211d1e0bd75d24a0813a35de50dda5f
vivekmitra <heater.mitra@gmail.com>
2022-01-09 02:45:21 +0530

    polling timeout fix

--- bot.py
+++ bot.py
@@ -79,4 +79,4 @@ def default(message):
     bot.reply_to(message, 'I did not understand '+'\N{confused face}')
 
 
-bot.bot.infinity_polling(timeout=10, long_polling_timeout = 5)
+bot.infinity_polling(timeout=10, long_polling_timeout = 5)
