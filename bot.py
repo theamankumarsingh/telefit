a9d89b26e13e126b5ba686ee13b9a816d5007bbb
Gautham <gauthamkrishnanpriya@gmail.com>
2022-01-09 08:59:11 +0530

    Completed stage-2

--- bot.py
+++ bot.py
@@ -4,16 +4,21 @@ import telebot
 import requests
 import json
 import csv
+import re
 
 # TODO: 1.1 Add Request HTTP URL of the API
 NUTRITIONIX_API_KEY = environ['NUTRITIONIX_API_KEY']
 NUTRITIONIX_APP_ID = environ['NUTRITIONIX_APP_ID']
 HTTP_API = environ['http_api']
+HTTP_URL_NUTRITION = "https://trackapi.nutritionix.com/v2/natural/nutrients"
+HTTP_URL_EXERCISE = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 headers = {'Content-Type': 'application/json',
            'x-app-id': NUTRITIONIX_APP_ID, 'x-app-key': NUTRITIONIX_API_KEY}
 user = {'name': None, 'gender': None,
         'weight': None, 'height': None, 'age': None}
+
+
 bot = telebot.TeleBot(HTTP_API)
 
 
@@ -43,9 +48,16 @@ def setUser(message):
     global user
     usr_input = message.text[6:]
     # TODO: 2.1 Set user data
+    user_data = usr_input.split(', ')
+    print(user_data)
+    user["name"] = user_data[0]
+    user["gender"] = user_data[1]
+    user["weight"] = user_data[2]
+    user["height"] = user_data[3]
+    user["age"] = user_data[4]
     bot.reply_to(message, 'User set!')
-    reply = ''
     # TODO: 2.2 Display user details in the telegram chat
+    reply = "Name: " + user["name"] + "\nGender: " + user["gender"] + "\nWeight: " + str(user["weight"]) + "\nHeight: " + str(user["height"]) + "\nAge: " + str(user["age"]) 
     bot.send_message(message.chat.id, reply)
 
 
@@ -53,7 +65,47 @@ def setUser(message):
 def getNutrition(message):
     bot.reply_to(message, 'Getting nutrition info...')
     # TODO: 1.2 Get nutrition information from the API
+    text = message.text[11:] 
+    print(text)
+    body = {
+        "query": text,
+        "timezone": "IN/Kolkata"
+    }
+    res = requests.post(HTTP_URL_NUTRITION,headers= headers, json= body)
+    if(res.status_code != 200):
+        print("Response Error!")
+        print(res.status_code)
+    
     # TODO: 1.3 Display nutrition data in the telegram chat
+    nutrient_list = ["food_name",
+            "serving_qty",
+            "nf_calories",
+            "nf_total_fat",
+            "nf_total_carbohydrate",
+            "nf_protein"]
+
+    nutrient_dict = {"serving_weight_grams":"Serving weight(gms)",
+            "food_name":"Food Name",
+            "serving_qty":"Quantity",
+            "nf_calories":"Calories",
+            "nf_total_fat":"Total Fat",
+            "nf_saturated_fat":"Saturated Fat",
+            "nf_cholesterol":"Cholestrol",
+            "nf_sodium":"Sodium",
+            "nf_total_carbohydrate":"Total Carbohydrate",
+            "nf_dietary_fiber":"Dietary Fibre",
+            "nf_sugars":"Sugars",
+            "nf_protein":"Protein",
+            "nf_potassium":"Potassium"}
+
+    res = res.json()
+    reply = ''
+    nutrient_values = res["foods"][0]
+    for nutrient in nutrient_list:
+        reply += nutrient_dict[nutrient] + ": " + str(nutrient_values[nutrient]) + "\n"
+    
+    print(reply)
+    bot.send_message(message.chat.id, reply)
     # TODO: 3.2 Dump data in a CSV file
 
 
@@ -61,7 +113,27 @@ def getNutrition(message):
 def getCaloriesBurn(message):
     bot.reply_to(message, 'Estimating calories burned...')
     # TODO: 2.3 Get exercise data from the API
+    body = {
+    "query": message.text[10:],
+    "gender": user["gender"],
+    "weight_kg": user["weight"],
+    "height_cm": user["height"],
+    "age": user["age"]
+    }
+    res = requests.post(HTTP_URL_EXERCISE,headers= headers, json= body)
+    if(res.status_code != 200):
+        print("Response Error!")
+        print(res.status_code)
+    res = res.json()
     # TODO: 2.4 Display exercise data in the telegram chat
+    Exname = str(res["exercises"][0]["name"])
+    ExDuration = str(res["exercises"][0]["duration_min"])
+    ExCalories = str(res["exercises"][0]["nf_calories"])
+    reply = "Exercise Name: " + Exname
+    reply += "\nDuration: " + ExDuration
+    reply += "\nCalories Burned: " + ExCalories
+    print(reply)
+    bot.send_message(message.chat.id, reply)
     # TODO: 3.3 Dump data in a CSV file
 
 
